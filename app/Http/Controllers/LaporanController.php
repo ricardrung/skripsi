b349c54e5e139addfb71d53224c8fef2e3232c58
Ricard Rungkat <richardrungkat03@gmail.com>
2025-05-24 23:57:00 +0800

    otw pembayaran

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -10,26 +10,5 @@ use App\Exports\PemasukanExport;
 class LaporanController extends Controller
 {
     //
-    public function download(Request $request)
-{
-    $query = Booking::with(['user', 'treatment'])
-        ->where('payment_status', 'sudah_bayar');
-
-    if ($request->filled('start_date')) {
-        $query->whereDate('booking_date', '>=', $request->start_date);
-    }
-
-    if ($request->filled('end_date')) {
-        $query->whereDate('booking_date', '<=', $request->end_date);
-    }
-
-    $data = $query->orderBy('booking_date')->get();
-
-    if ($request->format === 'excel') {
-        return Excel::download(new PemasukanExport($data), 'laporan_pemasukan.xlsx');
-    }
-
-    $pdf = Pdf::loadView('laporan.pemasukan_pdf', compact('data'));
-    return $pdf->download('laporan_pemasukan.pdf');
-}
+    
 }
